c672e16737b797f663e94298a5cb6850f08d2617
Tejas Sanap <sanap.tejas@gmail.com>
2021-10-09 17:12:27 +0530

    Add test for `discover` method.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,34 @@
-mod source_file;
+pub mod source_file;
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    use crate::source_file::*;
+    use dirtcrunch::Source;
+
+    #[tokio::test]
+    async fn it_works() {
+        let file = File::new();
+
+        let json = r#"
+{
+    "dataset_name": "some",
+    "format": "csv",
+    "url": "/app/some.csv",
+    "provider": {
+        "storage": "local"
+    }
+}
+        "#;
+
+        let config: serde_json::Value = serde_json::from_str(json).unwrap();
+
+        let catalog = file.discover(&config).await;
+
+        println!("{:?}", catalog);
+
+        assert!(
+            catalog[0]
+                .contains("\"properties\": {\"1\": {\"type\": \"string\"}, \"2\": {\"type\": \"string\"}, \"3\": {\"type\": \"string\"}, \"4\": {\"type\": \"string\"}}")
+        );
     }
 }
