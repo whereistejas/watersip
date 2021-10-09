08242e33c63c1b8adb81354a1c9e8492b72c76a2
Tejas Sanap <sanap.tejas@gmail.com>
2021-10-09 23:37:39 +0530

    Better comments in `build.rs`.

--- build.rs
+++ build.rs
@@ -4,11 +4,15 @@ use std::path::Path;
 
 #[tokio::main]
 async fn main() {
+    // Retrieve the SPECS for the `source-file` connector.
     let json = get_specs("airbyte/source-file").await;
+    // Create a struct called `File` and implement the `Source` for the `source-file` connector.
     let objects = create_objects("File", "airbyte/source-file", json);
+    // Create a .rs file with imports, connector structs and an implementation of the Source trait.
     let file = create_file(objects);
 
     let path = Path::new("src/source_file.rs");
 
+    // Write the source.rs file to disk.
     assert!(fs::write(path, &file).is_ok());
 }
