07bf5052752d0007086b17a15ac81795ca121427
Tejas Sanap <sanap.tejas@gmail.com>
2021-10-10 23:53:12 +0530

    Support new methods and add new tests for them

--- build.rs
+++ build.rs
@@ -1,76 +1,21 @@
-use dirtcrunch::{create_file, create_objects, get_specs};
-use futures::future::join_all;
-use std::fs;
-use std::path::Path;
-
-#[derive(serde::Deserialize, Debug)]
-struct Source {
-    name: String,
-    #[serde(rename(deserialize = "sourceDefinitionId"))]
-    source_definition_id: String,
-    #[serde(rename(deserialize = "dockerRepository"))]
-    docker_repository: String,
-    #[serde(rename(deserialize = "dockerImageTag"))]
-    docker_image_tag: String,
-    #[serde(rename(deserialize = "sourceType"))]
-    source_type: String,
-    #[serde(rename(deserialize = "documentationUrl"))]
-    documentation_url: String,
-}
+use dirtcrunch::{create_file, get_objects};
+use std::{env, fs};
 
 #[tokio::main]
 async fn main() {
     let sources_list =
         "airbyte/airbyte-config/init/src/main/resources/seed/source_definitions.yaml";
 
-    let sources: Vec<Source> = if Path::new(sources_list).exists() {
-        let file = fs::read_to_string(sources_list)
-            .expect("Could not read the list of source connectors.");
+    let file =
+        fs::read_to_string(sources_list).expect("Could not read the list of source connectors.");
 
-        let value: serde_yaml::Value = serde_yaml::from_str(&file).unwrap();
-        serde_yaml::from_value(value).unwrap()
-    } else {
-        panic!("Could not find the list of source connectors.")
-    };
+    let sources: serde_yaml::Value = serde_yaml::from_str(&file).unwrap();
 
-    let mut tasks = Vec::new();
+    let objects = get_objects(sources).await;
+    let file = create_file(objects);
 
-    for (count, source) in sources.iter().enumerate() {
-        if count == 3 {
-            break;
-        }
-        tasks.push(get_specs(&source.docker_repository))
-    }
+    let src_dir = format!("{}/src/", env::var("CARGO_MANIFEST_DIR").unwrap());
 
-    let specs = join_all(tasks).await;
-
-    let mut objects = Vec::new();
-
-    for (count, (spec, source)) in specs.iter().zip(sources.iter()).enumerate() {
-        if count == 3 {
-            break;
-        }
-
-        let iter = source.name.split_whitespace();
-
-        let mut name = String::new();
-        for (count, value) in iter.enumerate() {
-            if count == 3 {
-                break;
-            }
-            name.push_str(value);
-        }
-
-        objects.push(create_objects(
-            name.as_str(),
-            &source.docker_repository,
-            spec.clone(),
-        ))
-    }
-
-    let file = create_file(objects.join("\n"));
-
-    let path = Path::new("src/source.rs");
-    // Write the source.rs file to disk.
-    assert!(fs::write(path, &file).is_ok());
+    let source_path = format!("{}/sources.rs", src_dir);
+    assert!(fs::write(source_path, &file).is_ok());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,121 @@
-#[cfg(tests)]
-mod tests {}
+pub mod sources;
+
+use sources::File;
+
+pub mod file_commands {
+    use crate::File;
+    use dirtcrunch::{Docker, Source};
+    use futures::StreamExt;
+
+    pub async fn discover() -> String {
+        let file = File::new();
+
+        let json = r#"
+{
+    "dataset_name": "some",
+    "format": "csv",
+    "url": "/app/some.csv",
+    "provider": {
+        "storage": "local"
+    }
+}
+        "#;
+
+        let config: serde_json::Value = serde_json::from_str(json).unwrap();
+
+        let catalog = file.discover(&config).await;
+
+        catalog[0].clone()
+    }
+
+    pub async fn read() -> String {
+        let file = File::new();
+
+        let config_json = r#"
+{
+    "dataset_name": "some",
+    "format": "csv",
+    "url": "/app/some.csv",
+    "provider": {
+        "storage": "local"
+    }
+}
+        "#;
+
+        let catalog_json = r#"
+{
+    "streams": [
+        {
+            "stream": {
+                "name": "some",
+                "json_schema": {
+                    "$schema": "http://json-schema.org/draft-07/schema#",
+                    "type": "object",
+                    "properties": {
+                        "1": {
+                            "type": "number"
+                        },
+                        "2": {
+                            "type": "number"
+                        },
+                        "3": {
+                            "type": "number"
+                        },
+                        "4": {
+                            "type": "number"
+                        }
+                    }
+                }
+            },
+            "sync_mode": "full_refresh",
+            "destination_sync_mode": "overwrite"
+        }
+    ]
+}
+        "#;
+
+        let config: serde_json::Value = serde_json::from_str(config_json).unwrap();
+        let catalog: serde_json::Value = serde_json::from_str(catalog_json).unwrap();
+
+        let docker = Docker::new();
+        let mut reader = file.read(&docker, &config, &catalog).await;
+
+        let mut lines = String::new();
+
+        while let Some(blahblah) = reader.next().await {
+            lines.push_str(blahblah.as_str())
+        }
+
+        lines
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use file_commands::{discover, read};
+    use std::env;
+
+    #[tokio::test]
+    async fn test_discover() {
+        env::set_current_dir(env::var("CARGO_MANIFEST_DIR").unwrap());
+        let catalog = discover().await;
+
+        println!("{:?}", catalog);
+
+        assert!(
+            catalog
+                .contains(r#""properties": {"1": {"type": "number"}, "2": {"type": "number"}, "3": {"type": "number"}, "4": {"type": "number"}}"#)
+        );
+    }
+
+    #[tokio::test]
+    async fn test_read() {
+        env::set_current_dir(env::var("CARGO_MANIFEST_DIR").unwrap());
+        let lines = read().await;
+
+        println!("{:?}", lines);
+
+        assert!(lines.contains("\"type\": \"RECORD\""));
+    }
+}
